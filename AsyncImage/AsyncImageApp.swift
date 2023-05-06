9064831d408b225a5ec5c012b21b92319af66795
hweeying <jasminehweeying@gmail.com>
2023-05-06 15:11:17 +0800

    asyncimage

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Jasmine Lai Hweeying on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
